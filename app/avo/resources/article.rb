5e5fe6767789058b2eb973e1369b7f2a2664042e
Sébastien Dubois <sedubois@users.noreply.github.com>
2024-05-09 18:21:21 +0200

    rails g scaffold Article title:string body:rich_text && rails db:migrate

---
+++ app/avo/resources/article.rb
@@ -0,0 +1,12 @@
+class Avo::Resources::Article < Avo::BaseResource
+  self.includes = []
+  # self.search = {
+  #   query: -> { query.ransack(id_eq: params[:q], m: "or").result(distinct: false) }
+  # }
+
+  def fields
+    field :id, as: :id
+    field :title, as: :text
+    field :body, as: :text
+  end
+end
